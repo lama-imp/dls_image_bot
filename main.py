e83ee3a5a1620cd1347a869dbb92a0f7d7c2417a
lama_imp <lakeyeva.marina@gmail.com>
2022-01-26 12:36:59 +0400

    init

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from aiogram import executor
-
-from src.bot.bot import dp, on_startup
+from src.bot.bot import start_bot
 
 
 if __name__ == '__main__':
-    executor.start_webhook(
-        dispatcher=dp,
-        webhook_path='',
-        on_startup=on_startup,
-        skip_updates=True,
-        host="0.0.0.0",
-        port=3001,
-    )
+    start_bot()
